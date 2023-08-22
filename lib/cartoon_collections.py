3c6e4e5fd5a0af2948bf374a08f211091ff9234e
BreElle Wells <breellejordyn@gmail.com>
2023-08-22 15:49:13 -0600

    build function for length of calls

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -7,8 +7,12 @@ def summon_captain_planet(calls):
     return [call.capitalize() + "!" for call in calls]
 
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+
+    return False
 
 
 def find_the_cheese():
