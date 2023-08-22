9d02cebbe2a10635a988938651ca904cd6827688
BreElle Wells <breellejordyn@gmail.com>
2023-08-22 15:35:21 -0600

    print each dwarf's name

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,15 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for index, name in enumerate(names):
+        print(f"{index + 1}. {name}")
+
 
 def summon_captain_planet():
     pass
 
+
 def long_planeteer_calls():
     pass
+
 
 def find_the_cheese():
     pass
